b2a4d6cb26ab422478253c27809fc097527b1be3
alessandro.daviera <alessandro.daviera@ehealth.fgov.be>
2026-04-08 14:47:31 +0200

    add questionner to don't uses partition

--- src/main/java/be/fgov/ehealth/partitions/RequestPartitionHelperSvc.java
+++ src/main/java/be/fgov/ehealth/partitions/RequestPartitionHelperSvc.java
@@ -1,32 +1,25 @@
 package be.fgov.ehealth.partitions;
 
-import ca.uhn.fhir.interceptor.model.ReadPartitionIdRequestDetails;
-import ca.uhn.fhir.interceptor.model.RequestPartitionId;
-import ca.uhn.fhir.rest.api.server.RequestDetails;
-import ca.uhn.fhir.rest.server.exceptions.InternalErrorException;
-import jakarta.annotation.Nonnull;
-import jakarta.annotation.Nullable;
+import org.hl7.fhir.r4.model.codesystems.ResourceTypes;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class RequestPartitionHelperSvc extends ca.uhn.fhir.jpa.partition.RequestPartitionHelperSvc {
+
 	private static final Logger ourLog = LoggerFactory.getLogger(RequestPartitionHelperSvc.class);
 
-	public RequestPartitionHelperSvc(){
+	public RequestPartitionHelperSvc() {
+		super();
 		ourLog.info("Loaded be.fgov.ehealth.partitions.RequestPartitionHelperSvc");
 	}
 
-	@Nonnull
 	@Override
-	public RequestPartitionId determineReadPartitionForRequest(
-		@Nullable RequestDetails theRequest, @Nonnull ReadPartitionIdRequestDetails theDetails) {
-		RequestPartitionId requestPartitionId;
-		try{
-			requestPartitionId = super.determineReadPartitionForRequest(theRequest,theDetails);
-		} catch(InternalErrorException iee){
-			return RequestPartitionId.allPartitions();
+	public boolean isResourcePartitionable(final String theResourceType) {
+		if (ResourceTypes.QUESTIONNAIRE.toCode().equals(theResourceType)) {
+			return true;
 		}
 
-		return requestPartitionId;
+		return super.isResourcePartitionable(theResourceType);
 	}
+
 }
